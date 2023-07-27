59bc07414ed72ff31ea01e5fa5ece95a1cedf31d
sanseongKo <84113169+sanseongKo@users.noreply.github.com>
2023-07-27 09:42:22 +0900

    변경한 login request body 명 변경

--- src/main/java/com/example/toyProject/presentation/member/MemberController.java
+++ src/main/java/com/example/toyProject/presentation/member/MemberController.java
@@ -1,7 +1,8 @@
 package com.example.toyProject.presentation.member;
 
 import com.example.toyProject.application.member.MemberService;
-import com.example.toyProject.presentation.member.request.LoginDto;
+import com.example.toyProject.presentation.member.request.SignupRequestBody;
+import com.example.toyProject.presentation.member.request.LoginRequestBody;
 import com.example.toyProject.config.security.jwt.TokenInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +17,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public TokenInfo login(@RequestBody LoginDto loginDto) {
-        String memberId = loginDto.getMemberId();
-        String password = loginDto.getPassword();
+    public TokenInfo login(@RequestBody LoginRequestBody loginRequestBody) {
+        String memberId = loginRequestBody.getMemberId();
+        String password = loginRequestBody.getPassword();
 
         return memberService.login(memberId, password);
     }
